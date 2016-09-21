50b9a1a73a590b11e606b98506e664aa4226a377
sgalkina <galkina.s.a@gmail.com>
2016-09-21 16:43:47 +0200

    feat: POST instead of GET

--- genotype_to_model/app.py
+++ genotype_to_model/app.py
@@ -8,16 +8,16 @@ def create_app(settings_object):
     app = Flask(__name__)
     app.config.from_object(settings_object)
 
-    @app.route('/model/modify/genotype')
+    @app.route('/model/modify/genotype', methods=['POST'])
     def adjust_model():
         if 'model-id' in request.args:
-            model = model_by_id(request.args['model-id'])
+            model = model_by_id(request.values['model-id'])
         elif 'model' in request.args:
-            model = cobra.io.json.from_json(request.args['model'])
+            model = cobra.io.json.from_json(request.values['model'])
         else:
             return Response(response="No model is given", status=400)
         return Response(
-            response=cobra.io.json.to_json(apply_genotype_changes(model, request.args.getlist('genotype-changes'))),
+            response=cobra.io.json.to_json(apply_genotype_changes(model, request.values.getlist('genotype-changes'))),
             status=200,
             mimetype="application/json"
         )
