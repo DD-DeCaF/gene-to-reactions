1acc2784d430e25925a3af62d75907673e329824
sgalkina <galkina.s.a@gmail.com>
2016-10-20 15:32:57 +0200

    chore: logging handler

--- genotype_to_model/__init__.py
+++ genotype_to_model/__init__.py
@@ -1,7 +1,6 @@
 import logging
 import sys
 
-logging.basicConfig()
 logger = logging.getLogger('genotype-to-model')
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))  # Logspout captures logs from stdout if docker containers
 logger.setLevel(logging.INFO)
