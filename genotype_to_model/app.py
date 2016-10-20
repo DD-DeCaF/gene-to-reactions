8ea95b11d62de58a3b96ab35ad429d0bca0198eb
sgalkina <galkina.s.a@gmail.com>
2016-10-20 15:51:44 +0200

    chore: measure how much does it cost to load a model

--- genotype_to_model/app.py
+++ genotype_to_model/app.py
@@ -3,6 +3,7 @@ import cobra
 from cameo.core.solver_based_model import to_solver_based_model
 from genotype_to_model.strain_to_model import apply_genotype_changes
 from genotype_to_model.utils import model_by_id
+from genotype_to_model import logger
 
 
 def create_app(settings_object):
@@ -12,9 +13,13 @@ def create_app(settings_object):
     @app.route('/model/modify/genotype', methods=['POST'])
     def adjust_model():
         if 'model-id' in request.values:
+            logger.info('Got model from model-id {}'.format(request.values['model-id']))
             model = model_by_id(request.values['model-id'])
+            logger.info('Loaded model with model-id {}'.format(request.values['model-id']))
         elif 'model' in request.values:
+            logger.info('Got model in json')
             model = to_solver_based_model(cobra.io.json.from_json(request.values['model']))
+            logger.info('Loaded model from json')
         else:
             return Response(response="No model is given", status=400)
         return Response(
