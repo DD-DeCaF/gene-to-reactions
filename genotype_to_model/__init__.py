ecc4408af3f93eb6aec6a739392bdce36889af0e
Ali Kaafarani <ali@kvikshaug.no>
2018-01-02 18:14:09 +0100

    chore: implement raven capturing

--- genotype_to_model/__init__.py
+++ genotype_to_model/__init__.py
@@ -1,6 +1,19 @@
 import logging
 import sys
 
+from raven import Client
+from raven.conf import setup_logging
+from raven.handlers.logging import SentryHandler
+
+from . import settings
+
+
 logger = logging.getLogger('genotype-to-model')
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))  # Logspout captures logs from stdout if docker containers
 logger.setLevel(logging.INFO)
+
+# Configure Raven to capture warning logs
+raven_client = Client(settings.SENTRY_DSN)
+handler = SentryHandler(raven_client)
+handler.setLevel(logging.WARNING)
+setup_logging(handler)
